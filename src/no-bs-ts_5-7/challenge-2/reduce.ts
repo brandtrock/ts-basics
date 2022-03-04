6d7b7f3141b294209a9c3b40c2d7e9630ff170c6
Brandt Jorgensen <brandt.jorgensen@datasite.com>
2022-03-04 16:16:02 -0600

    Update reduce.ts

--- src/no-bs-ts_5-7/challenge-2/reduce.ts
+++ src/no-bs-ts_5-7/challenge-2/reduce.ts
@@ -4,6 +4,14 @@
  * Reproduce forEach, filter, and map using reduce and they have to be type safe
  */
 
-// FOR-EACH
 const myArr = [1, 2, 3];
+
+// REDUCE
+console.log("REDUCE");
+const initialVal = 0;
+const test = myArr.reduce((prev, curr) => prev + curr, initialVal);
+console.log(test);
+
+// FOR-EACH
+console.log("FOR EACH");
 myArr.forEach((n: number) => console.log(n * 10));
